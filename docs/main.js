bd215b673bf7a7464fcbc50baf1bb5df9ba676ad
DivinexRoyalty <shaunmassenburg9@gmail.com>
2022-12-05 15:43:01 -0800

    adding game,css and, java

--- docs/main.js
+++ docs/main.js
@@ -29,4 +29,15 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
   }
   
-  
+// OVERLAY
+
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+
